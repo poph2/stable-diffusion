16820c48cbe536364a8784b28e994da3475c161f
Adeniyi Bello <niyipopp@yahoo.co.uk>
2023-04-23 23:29:41 -0400

    Update txt_2_img.py

--- scripts/sd/txt_2_img.py
+++ scripts/sd/txt_2_img.py
@@ -205,7 +205,7 @@ class Text2Image:
                 x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
                 img = Image.fromarray(x_sample.astype(np.uint8))
                 # img = put_watermark(img, wm_encoder)
-                img.save(os.path.join(sample_path, f"{base_count:05}.png"))
+                img.save(os.path.join(sample_path, f"{base_count:05}_{i:03}.png"))
                 base_count += 1
 
         if not self.opt.skip_grid:
